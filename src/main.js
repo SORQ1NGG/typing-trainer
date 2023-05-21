d7ddd91ebed0c6bb6ecaa1004afc429ab5e8d3d6
SORQ1NGG <daniil.sork@gmail.com>
2023-05-22 01:38:30 +0300

    chore: R => r

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import App from './App.vue';
-import Router from './router';
+import router from './router';
 import { createApp } from 'vue';
 import '@/assets/css/style.scss';
 
 const app = createApp(App);
-app.use(Router);
+app.use(router);
 app.mount('#app');
