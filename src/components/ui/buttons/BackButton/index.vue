97f039b2400670a63422d8dbf3b05bb0db4d5c70
SORQ1NGG <daniil.sork@gmail.com>
2023-05-22 15:24:35 +0300

    feat: added back-button components

---
+++ src/components/ui/buttons/BackButton/index.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'BackButton',
+};
+</script>
+
+<template>
+    <button class="button-back">
+        Назад ➔
+    </button>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
