d7fb56e2e34b8b38ae9f44bb036703187e654c86
SORQ1NGG <daniil.sork@gmail.com>
2023-05-22 15:24:01 +0300

    feat: added loader

---
+++ src/components/CircleLoader/index.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'CircleLoader',
+};
+</script>
+
+<template>
+    <div class="lds-ring">
+        <div /><div /><div /><div />
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
